352701053f9f60f4d2965ee18c9553fa58703a11
6422781664-KrittidechPai <6422781664@g.siit.tu.ac.th>
2024-12-13 19:47:19 +0700

    Add ganche_cli as network choice

--- hardhat.config.js
+++ hardhat.config.js
@@ -4,9 +4,13 @@ module.exports = {
   solidity: "0.8.28",
   networks: {
     // Ganache GUI
-    ganache: {
+    ganache_gui: {
       url: "http://127.0.0.1:7545",
       chainId: 1337, // Default Ganache chain ID
+    },
+    ganache_cli: {
+      url: "http://127.0.0.1:8545",
+      chainId:1337
     },
   },
 };
